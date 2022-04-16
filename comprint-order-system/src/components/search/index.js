8906587b3ded710e3e658fd06a7bf62d5550f017
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-16 10:44:45 -0400

    fp search page ui

--- comprint-order-system/src/components/search/index.js
+++ comprint-order-system/src/components/search/index.js
@@ -3,25 +3,25 @@ import React from "react";
 const Search = () => {
     return(
         <>
-            <header className="alert-dark text-center fw-bold text-light p">
+            <header className="alert-dark text-center fw-bold text-light p-2">
                 Comprint Order System
             </header>
-            <h3 className="alert-info text-center text-dark fw-bold p-2">
+            <h4 className="alert-info text-dark fw-bold p-2">
                 Search Purchase Orders
-            </h3>
+            </h4>
 
 
-            <p className="form-label mt-4">Search by a term:</p>
+            <p className="form-label mt-4 left">Search by a term:</p>
             <div className="input-group">
-                <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                <select className="form-select " id="inputGroupSelect04" aria-label="Example select with button addon">
                     <option selected>Search by...</option>
                     <option value="PONumber">PO Number</option>
                     <option value="ClientName">Client Name</option>
                     <option value="DueDate">Due Date</option>
                     <option value="DateReceived">Date Received</option>
                 </select>
-                <input className="input-group-text col-10" placeholder="Search order system" />
-                <button className="btn btn-outline-info" type="button">Search</button>
+                <input className="input-group-text col-10 text-start" placeholder="Search order system" />
+                <button className="btn btn-info btn-dark fw-bold" type="button">Search</button>
             </div>
 
             <p className="form-label mt-4">View all orders:</p>
@@ -33,7 +33,7 @@ const Search = () => {
                     <option value="AllOrdersApproved">in production</option>
                     <option value="AllOrdersPrinted">ready to be invoiced</option>
                 </select>
-                <button className="btn btn-outline-info" type="button">View</button>
+                <button className="btn btn-info  fw-bold text-dark" type="button">View</button>
             </div>
 
         </>
