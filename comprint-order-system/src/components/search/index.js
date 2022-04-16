66f81a606e0d0473b187b48a8696025c72d233cf
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-16 10:53:01 -0400

    FP fixed forms in search

--- comprint-order-system/src/components/search/index.js
+++ comprint-order-system/src/components/search/index.js
@@ -10,32 +10,32 @@ const Search = () => {
                 Search Purchase Orders
             </h4>
 
+            <div className="container ps-3 pe-3">
+                <p className="form-label mt-4 left">Search by a term:</p>
+                <div className="input-group">
+                    <select className="form-select pb-2" id="inputGroupSelect04" aria-label="Example select with button addon">
+                        <option selected>Select</option>
+                        <option value="PONumber">PO Number</option>
+                        <option value="ClientName">Client Name</option>
+                        <option value="DueDate">Due Date</option>
+                        <option value="DateReceived">Date Received</option>
+                    </select>
+                    <input className="input-group-text col-9 text-start" placeholder="Search order system" />
+                    <button className="btn btn-info btn-dark fw-bold" type="button">Search</button>
+                </div>
 
-            <p className="form-label mt-4 left">Search by a term:</p>
-            <div className="input-group">
-                <select className="form-select " id="inputGroupSelect04" aria-label="Example select with button addon">
-                    <option selected>Search by...</option>
-                    <option value="PONumber">PO Number</option>
-                    <option value="ClientName">Client Name</option>
-                    <option value="DueDate">Due Date</option>
-                    <option value="DateReceived">Date Received</option>
-                </select>
-                <input className="input-group-text col-10 text-start" placeholder="Search order system" />
-                <button className="btn btn-info btn-dark fw-bold" type="button">Search</button>
+                <p className="form-label mt-4">View all orders:</p>
+                <div className="input-group">
+                    <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                        <option value="AllOrders" selected>All Orders</option>
+                        <option value="AllOrdersReceived">to be proofed</option>
+                        <option value="AllOrdersProofed">waiting for art approval</option>
+                        <option value="AllOrdersApproved">in production</option>
+                        <option value="AllOrdersPrinted">ready to be invoiced</option>
+                    </select>
+                    <button className="btn btn-info  fw-bold text-dark" type="button">View</button>
+                </div>
             </div>
-
-            <p className="form-label mt-4">View all orders:</p>
-            <div className="input-group">
-                <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-                    <option value="AllOrders" selected>All Orders</option>
-                    <option value="AllOrdersReceived">to be proofed</option>
-                    <option value="AllOrdersProofed">waiting for art approval</option>
-                    <option value="AllOrdersApproved">in production</option>
-                    <option value="AllOrdersPrinted">ready to be invoiced</option>
-                </select>
-                <button className="btn btn-info  fw-bold text-dark" type="button">View</button>
-            </div>
-
         </>
 
 
