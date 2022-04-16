82f368f9a977cb403b883671bfbf4d51232ba4d1
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-16 07:50:12 -0400

    fp Home page ui

--- comprint-order-system/src/components/home/index.js
+++ comprint-order-system/src/components/home/index.js
@@ -1,13 +1,33 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = () => {
     return(
         <>
-            <h2 className="alert-dark text-center font-monospace p">
-                Comprint Order System
-            </h2>
-        </>
+            <div className="container">
+                <img className="d-block mx-auto m-3" src='../images/comprint-logo.png' alt="comprintLogo"/>
+            </div>
+                <h2 className="alert-dark text-center fw-bold pt-1 pb-2 mb-3">
+                    Comprint Order System
+                </h2>
 
+
+                <Link to="search" className="d-grid col-5 container align-content-center">
+                    <button type="button" className="btn btn-info m-2 text-dark fw-bold">Search Orders</button>
+                </Link>
+
+                <Link to="login" className="d-grid col-5 container align-content-center">
+                    <button type="button" className="btn btn-secondary m-2 text-dark fw-bold">Log In</button>
+                </Link>
+
+                <Link to="profile" className="d-grid col-5 container align-content-center">
+                    <button type="button" className="btn btn-warning m-2 text-dark fw-bold">Profile</button>
+                </Link>
+
+                <Link to="detail" className="d-grid col-5 container align-content-center">
+                    <button type="button" className="btn btn-dark m-2 fw-bold">Get Shipping Rates</button>
+                </Link>
+        </>
 
     )
 
